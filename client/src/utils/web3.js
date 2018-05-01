f1d77cb5b30ff4ceb82a04422f0d62a38f41da80
Andy Chen <amazingandyyy@gmail.com>
2018-05-01 12:34:45 -0700

    add more instructions

--- client/src/utils/web3.js
+++ client/src/utils/web3.js
@@ -6,4 +6,8 @@ if(typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 }
 
+if(!web3 || !web3.eth){
+  window.alert('You need to install metamask(https://metamask.io/)~')
+}
+
 export default web3;
